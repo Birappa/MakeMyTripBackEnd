b0a8145179ef067be98ad2b720e4c08c99e44777
A S <sneha.a-s@capgemini.com>
2019-01-15 09:23:37 +0530

    project created

---
+++ src/main/java/com/makemytrip/server/MakeMyTripBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.makemytrip.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMyTripBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMyTripBackEndApplication.class, args);
+	}
+
+}
+
